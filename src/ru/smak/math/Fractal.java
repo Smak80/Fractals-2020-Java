d18f90f0fee58eb1c7247de8ef5cc31cc1f11fe6
Smak80 <smaklets@kpfu.ru>
2020-11-09 10:36:37 +0300

    Множество Мандельброта

---
+++ src/ru/smak/math/Fractal.java
@@ -0,0 +1,7 @@
+package ru.smak.math;
+
+import ru.smak.math.Complex;
+
+public interface Fractal {
+    boolean isInSet(Complex c);
+}
---
+++ src/ru/smak/math/Mandelbrot.java
@@ -0,0 +1,26 @@
+package ru.smak.math;
+
+public class Mandelbrot implements Fractal{
+
+    private int maxIters = 200;
+    private double r2 = 4;
+
+    public void setMaxIters(int value){
+        maxIters = Math.max(5, value);
+    }
+
+    public void setR(int value){
+        var r = Math.max(0.1, value);
+        r2 = r*r;
+    }
+
+    @Override
+    public boolean isInSet(Complex c) {
+        var z = new Complex();
+        for (int i = 0; i<maxIters; i++){
+            z = z.times(z).plus(c);
+            if (z.abs2() > r2) return false;
+        }
+        return true;
+    }
+}
