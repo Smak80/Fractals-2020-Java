1ea09f3d2c278d7aeb1e7dbeccc941acc13f6203
Smak80 <smaklets@kpfu.ru>
2020-11-09 10:23:38 +0300

    Создание пустого окна

---
+++ src/ru/smak/gui/MainWindow.java
@@ -0,0 +1,63 @@
+package ru.smak.gui;
+
+import ru.smak.gui.graphics.components.GraphicsPanel;
+import ru.smak.gui.graphics.coordinates.CartesianScreenPlane;
+import ru.smak.gui.graphics.coordinates.Converter;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MainWindow extends JFrame {
+    GraphicsPanel mainPanel;
+
+    static final Dimension MIN_SIZE = new Dimension(450, 350);
+    static final Dimension MIN_FRAME_SIZE = new Dimension(600, 500);
+
+    public MainWindow(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setMinimumSize(MIN_FRAME_SIZE);
+        setTitle("Полиномы");
+
+        mainPanel = new GraphicsPanel();
+
+        mainPanel.setBackground(Color.WHITE);
+
+        GroupLayout gl = new GroupLayout(getContentPane());
+        setLayout(gl);
+        gl.setVerticalGroup(gl.createSequentialGroup()
+                .addGap(4)
+                .addComponent(mainPanel, (int)(MIN_SIZE.height*0.8), MIN_SIZE.height, GroupLayout.DEFAULT_SIZE)
+                .addGap(4)
+        );
+        gl.setHorizontalGroup(gl.createSequentialGroup()
+                .addGap(4)
+                .addGroup(gl.createParallelGroup()
+                        .addComponent(mainPanel, MIN_SIZE.width, MIN_SIZE.width, GroupLayout.DEFAULT_SIZE)
+                )
+                .addGap(4)
+        );
+        pack();
+        var plane = new CartesianScreenPlane(
+                mainPanel.getWidth(),
+                mainPanel.getHeight(),
+                -2, 1, -1, 1
+        );
+
+        //mainPanel.addPainter(fp);
+
+        mainPanel.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                plane.setWidth(mainPanel.getWidth());
+                plane.setHeight(mainPanel.getHeight());
+            }
+        });
+        mainPanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                //mainPanel.repaint();
+            }
+        });
+    }
+}
